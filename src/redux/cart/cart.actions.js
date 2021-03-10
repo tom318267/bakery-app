25cc65e5105a827c9fa3385536d9e15d62e0175d
Thomas Rago <thomasrago@Thomass-iMac.fios-router.home>
2021-03-10 12:21:12 -0500

    aimplemented add and remove function to checkout component

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,4 +1,9 @@
-import { ADD_ITEM, TOGGLE_CART_HIDDEN } from "./cart.types";
+import {
+  ADD_ITEM,
+  TOGGLE_CART_HIDDEN,
+  CLEAR_ITEM_FROM_CART,
+  REMOVE_ITEM,
+} from "./cart.types";
 
 export const toggleCartHidden = () => ({
   type: TOGGLE_CART_HIDDEN,
@@ -6,5 +11,15 @@ export const toggleCartHidden = () => ({
 
 export const addItem = (item) => ({
   type: ADD_ITEM,
+  payload: item,
+});
+
+export const removeItem = (item) => ({
+  type: REMOVE_ITEM,
+  payload: item,
+});
+
+export const clearItemFromCart = (item) => ({
+  type: CLEAR_ITEM_FROM_CART,
   payload: item,
 });
